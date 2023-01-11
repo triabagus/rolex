295d30b973a2d7c55ff3f34bb7ea20569b88036f
triabagus <triatop9@gmail.com>
2023-01-11 20:34:21 +0700

    Cart components

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,6 +14,24 @@
 
 
 /*=============== SHOW CART ===============*/
+const cart = document.getElementById('cart'),
+        cartShop  = document.getElementById('cart-shop'),
+        cartClose = document.getElementById('cart-close')
 
+// CART SHOW
+// validate if constant exists
+if(cartShop){
+    cartShop.addEventListener('click', () =>{
+        cart.classList.add('show-cart')
+    })
+}
+
+// CART HIDDEN
+// validate if constant exists
+if(cartClose){
+    cartClose.addEventListener('click', () =>{
+        cart.classList.remove('show-cart')
+    })
+}
 
 /*=============== DARK LIGHT THEME ===============*/ 
