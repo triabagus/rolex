47d9d3dfad9e7dd913b193df2f594e1f6e68a208
triabagus <triatop9@gmail.com>
2023-01-13 21:16:08 +0700

    Change background header

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,15 @@
 /*=============== REMOVE MENU MOBILE ===============*/
 
 
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader = () => {
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    this.scrollY >= 50 ? header.classList.add('scroll-header')
+                    : header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
 /*=============== TESTIMONIAL SWIPER ===============*/
 
 
