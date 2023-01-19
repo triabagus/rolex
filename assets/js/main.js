09a39a5a7303de78855c5dfd0efdadb277e1a63a
triabagus <triatop9@gmail.com>
2023-01-19 21:18:47 +0700

    Swiper js update

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,25 +14,10 @@ const scrollHeader = () => {
 window.addEventListener('scroll', scrollHeader)
 
 /*=============== TESTIMONIAL SWIPER ===============*/
-// let testimonialSwiper = new Swiper(".testimonial-swiper", {
-//   spaceBetween: 30,
-//   loop: true,
-
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   },
-// });
-var swiper = new Swiper(".testimonial-swiper", {
-  slidesPerView: 3,
+let testimonialSwiper = new Swiper(".testimonial-swiper", {
   spaceBetween: 30,
-  slidesPerGroup: 3,
   loop: true,
-  loopFillGroupWithBlank: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
+
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
