d636fe146e66caaa4b92062adafcdc0398ae5c60
triabagus <triatop9@gmail.com>
2023-01-19 21:26:50 +0700

    Menu Show Hidden Js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,21 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+        navToggle   = document.getElementById('nav-toggle'),
+        navClose    = document.getElementById('nav-close')
 
+// Menu Show
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
 
+// Menu Hidden
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
 /*=============== REMOVE MENU MOBILE ===============*/
 
 
