a3dbfdd23842ef37f5f0c61ae5b7509293593517
triabagus <triatop9@gmail.com>
2023-01-20 08:34:37 +0700

    Remove menu mobile after click nav__link

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,8 +16,16 @@ if(navClose){
         navMenu.classList.remove('show-menu')
     })
 }
-/*=============== REMOVE MENU MOBILE ===============*/
 
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 const scrollHeader = () => {
